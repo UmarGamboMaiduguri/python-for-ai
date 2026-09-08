fe85eed0f19d6a8eec48d5dec9040cd3ee2143c9
Umar Gambo <gumar9000@gmail.com>
2026-09-08 16:26:39 +0100

    Add Python loops practice

---
+++ 01-python-basics/05_loops.py
@@ -0,0 +1,24 @@
+# Python Loops Practice
+# Author: Umar Gambo
+
+print("--- For Loop ---")
+
+for number in range(1, 6):
+    print("Number:", number)
+
+
+print("\n--- While Loop ---")
+
+count = 1
+
+while count <= 5:
+    print("Count:", count)
+    count += 1
+
+
+print("\n--- Student Scores ---")
+
+scores = [85, 72, 65, 90, 78]
+
+for score in scores:
+    print("Score:", score)
