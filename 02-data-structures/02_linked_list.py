9b8930d6a9a1d39e2f3f8ebfab720aa850f636a0
Umar Gambo <gumar9000@gmail.com>
2026-09-09 13:03:12 +0100

    Add linked list data structure practice

---
+++ 02-data-structures/02_linked_list.py
@@ -0,0 +1,46 @@
+# Linked List Practice
+# Author: Umar Gambo
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+
+        current = self.head
+
+        while current.next:
+            current = current.next
+
+        current.next = new_node
+
+    def display(self):
+        current = self.head
+
+        while current:
+            print(current.data, end=" -> ")
+            current = current.next
+
+        print("None")
+
+
+students = LinkedList()
+
+students.append("Aisha")
+students.append("Ibrahim")
+students.append("Fatima")
+
+print("--- Student Linked List ---")
+students.display()
